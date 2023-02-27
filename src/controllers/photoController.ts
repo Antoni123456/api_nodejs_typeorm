8de0e9569a577ed91b77ad07b2daba883714bb5f
Antoni123456 <antoni@nomade.mg>
2023-02-27 11:14:35 +0300

    add sytem pagination

--- src/controllers/photoController.ts
+++ src/controllers/photoController.ts
@@ -3,7 +3,11 @@ import { AppDataSource } from "../config/dbConfig";
 import { Photo } from "../entities/Photo";
 import { Author } from "../entities/Author";
 
-// Enregistrement des données
+/**
+ * Enregistrement des données
+ * @param req
+ * @param res
+ */
 const savePhoto = async (req: Request, res: Response) => {
   try {
     if (req.body) {
@@ -28,7 +32,6 @@ const savePhoto = async (req: Request, res: Response) => {
       // save in datbase
       await AppDataSource.getRepository(Photo).save(photo);
 
-
       res.status(201).json(photo);
     }
   } catch (error) {
@@ -36,49 +39,86 @@ const savePhoto = async (req: Request, res: Response) => {
   }
 };
 
-// Parcourir les données
-const getAllPhotos = async (req: Request, res: Response) => {
-  const allPhotos = await AppDataSource.getRepository(Photo)
-    .createQueryBuilder("photo")
-    .innerJoinAndSelect("photo.author", "author")
-    .getMany();
+// Systeme de pagination coté serveur
+const getPagination = (data: any[], page: number, limit: number) => {
+  const totalItems: number = data[1];
+  const photos = data[0];
+  const totalPages = Math.ceil(totalItems / limit);
+  const currentPage = page ? page : 0;
 
-  if (allPhotos) {
-    res.status(200).json(allPhotos);
+  return { totalItems, photos, totalPages, currentPage };
+};
+
+/**
+ * Parcourir des photos avec pagination
+ * @param req
+ * @param res
+ */
+const getAllPhotos = async (req: Request, res: Response) => {
+  // Récuperation de la nombre de page et taille via client
+  const { page, size } = req.body;
+
+  // Calcule des élément à ignorer
+  const offset = page ? +page * +size : 0;
+
+  // Récupération des photos existe dans la base
+  const allPhotosAndCount = await AppDataSource.getRepository(
+    Photo
+  ).findAndCount({
+    relations: {
+      author: true,
+    },
+    take: +size,
+    skip: offset,
+  });
+
+  if (allPhotosAndCount) {
+    const response = getPagination(allPhotosAndCount, page, size);
+    res.status(200).json(response);
   } else {
     res.status(400).json("Data not found!");
   }
 };
 
-// Mise à jour
+/**
+ * Mise à jour
+ * @param req
+ * @param res
+ */
 const editPhoto = async (req: Request, res: Response) => {
   const photoRepository = AppDataSource.getRepository(Photo);
 
-  if (req.params.id) {
-    const photoToUpdate: any = await photoRepository.findOneBy({
-      id: +req.params.id,
-    });
+  const photoToUpdate: any = await photoRepository.findOneBy({
+    id: +req.params.id,
+  });
 
+  if (photoToUpdate) {
     photoToUpdate.name = req.body.name;
     await photoRepository.save(photoToUpdate);
 
-    res.json(photoToUpdate);
+    res.status(200).json(photoToUpdate);
   }
 };
 
-// Suppression
+/**
+ * Suppression
+ * @param req
+ * @param res
+ */
 const deletePhoto = async (req: Request, res: Response) => {
-  const idPhoto: number = +req.params.id;
+  try {
+    const idPhoto: number = +req.params.id;
 
-  if (idPhoto) {
     const photoToRemove = await AppDataSource.getRepository(Photo).findBy({
       id: idPhoto,
     });
 
     if (photoToRemove) {
       await AppDataSource.getRepository(Photo).remove(photoToRemove);
-      res.json("Delete data successfully");
+      res.status(200).json({ message: "Delete data successfully!" });
     }
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
 
